e510c8915e7e097f523a71f9d21b7c398bc8f542
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-07-10 18:15:24 +0300

    //Writing binary data

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
@@ -12,19 +14,39 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    try {
+    public static void main(String[] args) {//Writing binary data
+        try (FileOutputStream binFile = new FileOutputStream("data.dat");
+             FileChannel binChannnel = binFile.getChannel()) {
+
+            byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numbytes = binChannnel.write(buffer);
+            System.out.println("numbytes written was " + numbytes);
+
+            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+            intBuffer.putInt(245);
+            intBuffer.flip();
+            numbytes = binChannnel.write(intBuffer);
+            System.out.println("numbytes written was " + numbytes);
+
+
+            intBuffer.flip();
+            intBuffer.putInt(-98765);
+            intBuffer.flip();
+            numbytes = binChannnel.write(intBuffer);
+            System.out.println("numbytes written was " + numbytes);
+
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            Files.write(dataPath, "\nLine 2".getBytes("UTF-8"), StandardOpenOption.APPEND);
-            List<String> lines = Files.readAllLines(dataPath);
-            for(String line : lines) {
-                System.out.println(line);
-            }
+//            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+//            Files.write(dataPath, "\nLine 2".getBytes("UTF-8"), StandardOpenOption.APPEND);
+//            List<String> lines = Files.readAllLines(dataPath);
+//            for(String line : lines) {
+//                System.out.println(line);
+//            }
 
-        } catch(IOException e) {
-	        e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
